6ad88278583e91190857fb32c20ed159796122e5
Nekrofage <lesanglierdesardennes@gmail.com>
2023-03-26 00:03:18 +0100

    [Add] Enemies

--- RoguelikeSpaceMarineAI.c
+++ RoguelikeSpaceMarineAI.c
@@ -8,6 +8,11 @@
 #define MAX_ROOM_SIZE 1024
 #define MAX_OBJECTS 10
 
+#define MAX_ENEMIES 10
+#define PLAYER_HP 20
+#define ENEMY_HP 10
+#define ENEMY_DAMAGE 8
+
 #define MAP_WIDTH 3
 #define MAP_HEIGHT 3
 
@@ -29,7 +34,17 @@ typedef struct {
   position pos;
   char symbol;
   char name[20];
+  int hp;
 } player;
+
+
+typedef struct {
+  position pos;
+  char symbol;
+  int hp;
+  int damage;
+} enemy;
+
 
 void load_room(const char* filename, room* m) {
   FILE* fp;
@@ -77,6 +92,43 @@ void draw_player(player* p) {
 }
 
 
+void draw_enemy(enemy* e, int i) {
+  mvaddch(e[i].pos.y, e[i].pos.x, e[i].symbol);
+}
+
+int combat(player* p, enemy* e) {
+  while (1) {
+    clear();
+    mvprintw(0, 0, "Combat phase!\n");
+    mvprintw(1, 0, "Player HP: %d\n", p->hp);
+    mvprintw(2, 0, "Enemy HP: %d\n", e->hp);
+    mvprintw(3, 0, "Press any key to attack!");
+
+    int combat_ch = getch();
+    int player_damage = rand() % 5 + 1;
+    int enemy_damage = rand() % 3 + 1;
+    if (combat_ch != ERR) {
+      e->hp -= player_damage;
+      if (e->hp <= 0) {
+        return 1;
+      }
+      p->hp -= enemy_damage;
+      if (p->hp <= 0) {
+        return -1;
+      }
+    }
+    refresh();
+  }
+}
+
+int check_for_enemy(player* p, enemy* e) {
+  if (p->pos.x == e->pos.x && p->pos.y == e->pos.y) {
+    return 1;
+  }
+  return 0;
+}
+
+
 int main() {
   initscr();
   raw();
@@ -84,36 +136,36 @@ int main() {
   noecho();
   curs_set(0);
 
-  player p = {{10, 10}, '@', ""};
+  player p = {{10, 10}, '@', "", PLAYER_HP};
   char name[50];
 
-     // Welcome Screen
+  // Welcome Screen
+  printw("Roguelike\n\n");
+  printw("Quel est ton nom, aventurier ? ");
+  refresh();
+  echo();
+  getstr(p.name);
+  noecho();
+
+
+  int choice = 0;
+  while (1) {
+    clear();
+
     printw("Roguelike\n\n");
-    printw("Quel est ton nom, aventurier ? ");
+    printw("1. Partons à l'aventure !!\n");
+    printw("2. Quitter\n");
     refresh();
-    echo();
-    getstr(p.name);
-    noecho();
 
+    choice = getch() - '0';
 
-    int choice = 0;
-    while (1) {
-        clear();
-
-        printw("Roguelike\n\n");
-        printw("1. Partons à l'aventure !!\n");
-        printw("2. Quitter\n");
-        refresh();
-
-        choice = getch() - '0';
-
-        if (choice == 1) {
-            break;
-        } else if (choice == 2) {
-            endwin();
-            return 0;
-        }
+    if (choice == 1) {
+      break;
+    } else if (choice == 2) {
+      endwin();
+      return 0;
     }
+  }
 
 
 
@@ -132,6 +184,13 @@ int main() {
   load_room("room7.txt", &m[7]);
   load_room("room8.txt", &m[8]);
 
+
+  // Initialize enemies
+  enemy e1 = {{20, 5}, 'E', ENEMY_HP, ENEMY_DAMAGE};
+  enemy e2 = {{40, 15}, 'E', ENEMY_HP, ENEMY_DAMAGE};
+  enemy e3 = {{5, 18}, 'E', ENEMY_HP, ENEMY_DAMAGE};
+
+
   while (1) {
     clear();
 
@@ -144,6 +203,12 @@ int main() {
     draw_room(&m[current_room]);
     draw_player(&p);
 
+    if (current_room == 0) {
+      draw_enemy(&e1, 0);
+      draw_enemy(&e2, 0);
+      draw_enemy(&e3, 0);
+    }
+
     int ch = getch();
     switch (ch) {
       case KEY_UP:
@@ -152,7 +217,7 @@ int main() {
           p.pos.y = ROOM_HEIGHT - 1;
         }
 
-        if (p.pos.y > 0 && m[current_room].data[p.pos.y - 1][p.pos.x] != '#') {
+        if (p.pos.y > 0 && m[current_room].data[p.pos.y - 1][p.pos.x] != '#' && !check_for_enemy(&p, &e1) && !check_for_enemy(&p, &e2) && !check_for_enemy(&p, &e3)) {
           if (m[current_room].data[p.pos.y - 1][p.pos.x] == '*') {
             if (p.pos.y > 1 && m[current_room].data[p.pos.y - 2][p.pos.x] != '#' && m[current_room].data[p.pos.y - 2][p.pos.x] != '*') {
               m[current_room].data[p.pos.y - 2][p.pos.x] = '*';
@@ -171,7 +236,7 @@ int main() {
           p.pos.y = 0;
         }
 
-        if (p.pos.y < ROOM_HEIGHT - 1 && m[current_room].data[p.pos.y + 1][p.pos.x] != '#') {
+        if (p.pos.y < ROOM_HEIGHT - 1 && m[current_room].data[p.pos.y + 1][p.pos.x] != '#' && !check_for_enemy(&p, &e1) && !check_for_enemy(&p, &e2) && !check_for_enemy(&p, &e3)) {
           if (m[current_room].data[p.pos.y + 1][p.pos.x] == '*') {
             if (p.pos.y < ROOM_HEIGHT - 2 && m[current_room].data[p.pos.y + 2][p.pos.x] != '#' && m[current_room].data[p.pos.y + 2][p.pos.x] != '*') {
               m[current_room].data[p.pos.y + 2][p.pos.x] = '*';
@@ -191,7 +256,7 @@ int main() {
           p.pos.x = ROOM_WIDTH - 1;
         }
 
-        if (p.pos.x > 0 && m[current_room].data[p.pos.y][p.pos.x - 1] != '#') {
+        if (p.pos.x > 0 && m[current_room].data[p.pos.y][p.pos.x - 1] != '#' && !check_for_enemy(&p, &e1) && !check_for_enemy(&p, &e2) && !check_for_enemy(&p, &e3)) {
           if (m[current_room].data[p.pos.y][p.pos.x - 1] == '*') {
             if (p.pos.x > 1 && m[current_room].data[p.pos.y][p.pos.x - 2] != '#' && m[current_room].data[p.pos.y][p.pos.x - 2] != '*') {
               m[current_room].data[p.pos.y][p.pos.x - 2] = '*';
@@ -209,7 +274,7 @@ int main() {
           current_room = current_room + 1;
           p.pos.x = 0;
         }
-        if (p.pos.x < ROOM_WIDTH - 1 && m[current_room].data[p.pos.y][p.pos.x + 1] != '#') {
+        if (p.pos.x < ROOM_WIDTH - 1 && m[current_room].data[p.pos.y][p.pos.x + 1] != '#' && !check_for_enemy(&p, &e1) && !check_for_enemy(&p, &e2) && !check_for_enemy(&p, &e3)) {
           if (m[current_room].data[p.pos.y][p.pos.x + 1] == '*') {
             if (p.pos.x < ROOM_WIDTH - 2 && m[current_room].data[p.pos.y][p.pos.x + 2] != '#' && m[current_room].data[p.pos.y][p.pos.x + 2] != '*') {
               m[current_room].data[p.pos.y][p.pos.x + 2] = '*';
@@ -227,6 +292,29 @@ int main() {
         exit(0);
         break;
     }
+
+    // Check if player is in combat with an enemy
+    if (check_for_enemy(&p, &e1)) {
+      combat(&p, &e1);
+      if (e1.hp <= 0) {
+        e1.pos.x = -1;
+        e1.pos.y = -1;
+      }
+    } else if (check_for_enemy(&p, &e2)) {
+      combat(&p, &e2);
+      if (e2.hp <= 0) {
+        e2.pos.x = -1;
+        e2.pos.y = -1;
+      }
+    } else if (check_for_enemy(&p, &e3)) {
+      combat(&p, &e3);
+      if (e3.hp <= 0) {
+        e3.pos.x = -1;
+        e3.pos.y = -1;
+      }
+    }
+
+
   }
 
   endwin();
